e790bb2d7dc9cb3a5be3f9ec2e3a9b9027b29b10
Stephen Sennett <ssennett@ssennett.net>
2024-03-26 11:30:30 +1100

    Moving CORS Headers to utils

--- src/utils.py
+++ src/utils.py
@@ -38,3 +38,10 @@ def hash_request(request):
 
     return hash.hexdigest()
 
+cors_headers = {
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS,POST",
+    "Access-Control-Expose-Headers": "X-Amzn-Trace-Id",
+}
+
